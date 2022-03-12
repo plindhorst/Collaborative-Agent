afff281cf533d303a3f856464306c717805caa1f
p.benschop@student.tudelft.nl <p.benschop@student.tudelft.nl>
2022-03-12 18:14:44 +0100

    Added moving thigns

--- agents1/BW4TBaselineAgent.py
+++ agents1/BW4TBaselineAgent.py
@@ -1,6 +1,6 @@
+from turtle import st
 from typing import final, List, Dict, Final
-import enum
-import random
+import enum, random
 from bw4t.BW4TBrain import BW4TBrain
 from matrx.agents.agent_utils.state import State
 from matrx.agents.agent_utils.navigator import Navigator
@@ -9,19 +9,15 @@ from matrx.actions.door_actions import OpenDoorAction
 from matrx.actions.object_actions import GrabObject, DropObject
 from matrx.messages.message import Message
 
-
 class Phase(enum.Enum):
-    PLAN_PATH_TO_CLOSED_DOOR = (1,)
-    FOLLOW_PATH_TO_CLOSED_DOOR = (2,)
-    OPEN_DOOR = 3
-    SEARCH_ROOM = 4
-    FOUND_GOAL_BLOCK = 5
-    PICK_UP_GOAL_BLOCK = 6
-    DROP_GOAL_BLOCK = 7
+    PLAN_PATH_TO_CLOSED_DOOR=1,
+    FOLLOW_PATH_TO_CLOSED_DOOR=2,
+    OPEN_DOOR=3
 
 
 class BaseLineAgent(BW4TBrain):
-    def __init__(self, settings: Dict[str, object]):
+
+    def __init__(self, settings:Dict[str,object]):
         super().__init__(settings)
         self._phase = Phase.PLAN_PATH_TO_CLOSED_DOOR
         self._teamMembers = []
@@ -29,91 +25,79 @@ class BaseLineAgent(BW4TBrain):
     def initialize(self):
         super().initialize()
         self._state_tracker = StateTracker(agent_id=self.agent_id)
-        self._navigator = Navigator(
-            agent_id=self.agent_id,
-            action_set=self.action_set,
-            algorithm=Navigator.A_STAR_ALGORITHM,
-        )
+        self._navigator = Navigator(agent_id=self.agent_id, 
+            action_set=self.action_set, algorithm=Navigator.A_STAR_ALGORITHM)
 
     def filter_bw4t_observations(self, state):
         return state
 
-    def decide_on_bw4t_action(self, state: State):
-        agent_name = state[self.agent_id]["obj_id"]
+    def decide_on_bw4t_action(self, state:State):
+        agent_name = state[self.agent_id]['obj_id']
         # Add team members
-        for member in state["World"]["team_members"]:
-            if member != agent_name and member not in self._teamMembers:
-                self._teamMembers.append(member)
+        for member in state['World']['team_members']:
+            if member!=agent_name and member not in self._teamMembers:
+                self._teamMembers.append(member)       
         # Process messages from team members
         receivedMessages = self._processMessages(self._teamMembers)
         # Update trust beliefs for team members
         self._trustBlief(self._teamMembers, receivedMessages)
-
+        
         while True:
-            if Phase.PLAN_PATH_TO_CLOSED_DOOR == self._phase:
+            if Phase.PLAN_PATH_TO_CLOSED_DOOR==self._phase:
                 self._navigator.reset_full()
-                closedDoors = [
-                    door
-                    for door in state.values()
-                    if "class_inheritance" in door
-                    and "Door" in door["class_inheritance"]
-                    and not door["is_open"]
-                ]
-                if len(closedDoors) == 0:
+                closedDoors = [door for door in state.values()
+                    if 'class_inheritance' in door and 'Door' in door['class_inheritance'] and not door['is_open']]
+                if len(closedDoors)==0:
                     return None, {}
                 # Randomly pick a closed door
                 self._door = random.choice(closedDoors)
-                doorLoc = self._door["location"]
+                doorLoc = self._door['location']
                 # Location in front of door is south from door
-                doorLoc = doorLoc[0], doorLoc[1] + 1
+                doorLoc = doorLoc[0],doorLoc[1]+1
                 # Send message of current action
-                self._sendMessage(
-                    "Moving to door of " + self._door["room_name"], agent_name
-                )
+                self._sendMessage('Moving to door of ' + self._door['room_name'], agent_name)
                 self._navigator.add_waypoints([doorLoc])
-                self._phase = Phase.FOLLOW_PATH_TO_CLOSED_DOOR
+                self._phase=Phase.FOLLOW_PATH_TO_CLOSED_DOOR
 
-            if Phase.FOLLOW_PATH_TO_CLOSED_DOOR == self._phase:
+            if Phase.FOLLOW_PATH_TO_CLOSED_DOOR==self._phase:
                 self._state_tracker.update(state)
                 # Follow path to door
                 action = self._navigator.get_move_action(self._state_tracker)
-                if action != None:
-                    return action, {}
-                self._phase = Phase.OPEN_DOOR
+                if action!=None:
+                    return action, {}   
+                self._phase=Phase.OPEN_DOOR
 
-            if Phase.OPEN_DOOR == self._phase:
-                # self._phase = Phase.PLAN_PATH_TO_CLOSED_DOOR
-                self._phase = Phase.SEARCH_ROOM
+            if Phase.OPEN_DOOR==self._phase:
+                self._phase=Phase.PLAN_PATH_TO_CLOSED_DOOR
                 # Open door
-                return OpenDoorAction.__name__, {"object_id": self._door["obj_id"]}
-            if Phase.SEARCH_ROOM == self._phase:
-            # TODO: walk through the whole room and observe what kind of objects are there
+                return OpenDoorAction.__name__, {'object_id':self._door['obj_id']}
+    
 
     def _sendMessage(self, mssg, sender):
-        """
+        '''
         Enable sending messages in one line of code
-        """
+        '''
         msg = Message(content=mssg, from_id=sender)
         if msg.content not in self.received_messages:
             self.send_message(msg)
 
     def _processMessages(self, teamMembers):
-        """
+        '''
         Process incoming messages and create a dictionary with received messages from each team member.
-        """
+        '''
         receivedMessages = {}
         for member in teamMembers:
             receivedMessages[member] = []
         for mssg in self.received_messages:
             for member in teamMembers:
                 if mssg.from_id == member:
-                    receivedMessages[member].append(mssg.content)
+                    receivedMessages[member].append(mssg.content)       
         return receivedMessages
 
     def _trustBlief(self, member, received):
-        """
+        '''
         Baseline implementation of a trust belief. Creates a dictionary with trust belief scores for each team member, for example based on the received messages.
-        """
+        '''
         # You can change the default value to your preference
         default = 0.5
         trustBeliefs = {}
@@ -121,7 +105,7 @@ class BaseLineAgent(BW4TBrain):
             trustBeliefs[member] = default
         for member in received.keys():
             for message in received[member]:
-                if "Found" in message and "colour" not in message:
-                    trustBeliefs[member] -= 0.1
+                if 'Found' in message and 'colour' not in message:
+                    trustBeliefs[member]-=0.1
                     break
         return trustBeliefs
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bw4t.BW4TWorld import BW4TWorld
 from bw4t.statistics import Statistics
+from agents1.Group58Agent import Group58Agent
 from agents1.BW4TBaselineAgent import BaseLineAgent
 from agents1.BW4THuman import Human
 
@@ -11,8 +12,8 @@ press the start button in god mode to start the session.
 
 if __name__ == "__main__":
     agents = [
-        {"name": "agent1", "botclass": BaseLineAgent, "settings": {}},
-        {"name": "agent2", "botclass": BaseLineAgent, "settings": {}},
+        #{"name": "agent1", "botclass": BaseLineAgent, "settings": {}},
+        {"name": "agent2", "botclass": Group58Agent, "settings": {}},
         {"name": "human", "botclass": Human, "settings": {}},
     ]
 
