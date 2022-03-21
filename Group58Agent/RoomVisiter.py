c2c2a393a0f6355490156e0876b9787f614b806e
p.benschop@student.tudelft.nl <p.benschop@student.tudelft.nl>
2022-03-21 15:16:07 +0100

    Update method

--- Group58Agent/RoomVisiter.py
+++ Group58Agent/RoomVisiter.py
@@ -36,14 +36,14 @@ class RoomVisiter:
 
     # Update the goal blocks seen in agent range
     def _update_room(self):
-        blocks = self.agent.state.get_with_property({'is_collectable': True})
-        if blocks is not None:
-            # Go over each block in the field of view
-            for block in blocks:
-                block = {"colour": block["visualization"]["colour"], "shape": block["visualization"]["shape"],
-                         "location": block["location"], "size": block["visualization"]["size"]}
-                # Check if the block is a goal block
-                if not self.agent.settings["colourblind"]:
+        if not self.agent.settings["colourblind"]:
+            blocks = self.agent.state.get_with_property({'is_collectable': True})
+            if blocks is not None:
+                # Go over each block in the field of view
+                for block in blocks:
+                    block = {"colour": block["visualization"]["colour"], "shape": block["visualization"]["shape"],
+                            "location": block["location"], "size": block["visualization"]["size"]}
+                    # Check if the block is a goal block
                     for drop_off in self.agent.drop_offs:
                         if block["colour"] == drop_off["colour"] and block["shape"] == drop_off["shape"]:
                             if block not in self.found_goal_blocks:
